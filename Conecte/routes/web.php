2416bde61d0aff6452afa924116671c4ddb078ec
lucas-catto <lucascattosilva2024@gmail.com>
2025-09-27 15:44:32 -0300

    Criar grupos de rotas por controller.

--- Conecte/routes/web.php
+++ Conecte/routes/web.php
@@ -1,7 +1,55 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CaregiverController;
+use App\Http\Controllers\ContractController;
+use App\Http\Controllers\ContractorController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(HomeController::class)->prefix('/')->group(function() {
+
 });
+
+Route::controller(AuthController::class)->prefix('auth')->group(function() {
+
+});
+
+Route::controller(UserController::class)->prefix('user')->group(function() {
+    Route::get('', 'index')->middleware([]);
+    Route::get('create', 'create')->middleware([]);
+    Route::post('', 'store')->middleware([]);
+    Route::get('show/{}', 'show')->middleware([]);
+    Route::get('edit/{}', 'edit')->middleware([]);
+    Route::put('', 'update')->middleware([]);
+    Route::delete('', 'destroy')->middleware([]);
+});
+
+Route::controller(ContractorController::class)->prefix('contractor')->group(function() {
+
+});
+
+Route::controller(CaregiverController::class)->prefix('caregiver')->group(function() {
+
+});
+
+Route::controller(ContractController::class)->prefix('contract')->group(function() {
+    Route::get('', 'index')->middleware([]);
+    Route::get('create', 'create')->middleware([]);
+    Route::post('', 'store')->middleware([]);
+    Route::get('show/{}', 'show')->middleware([]);
+    Route::get('edit/{}', 'edit')->middleware([]);
+    Route::put('', 'update')->middleware([]);
+    Route::delete('', 'destroy')->middleware([]);
+});
+
+/*
+    Route::get('', 'index')->middleware([]);
+    Route::get('create', 'create')->middleware([]);
+    Route::post('', 'store')->middleware([]);
+    Route::get('show/{}', 'show')->middleware([]);
+    Route::get('edit/{}', 'edit')->middleware([]);
+    Route::put('', 'update')->middleware([]);
+    Route::delete('', 'destroy')->middleware([]);
+ */
